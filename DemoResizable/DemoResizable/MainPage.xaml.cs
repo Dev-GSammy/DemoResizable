6ef3f4a77371be00aeebb02e5763e946e63a6ba5
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2022-08-15 19:57:30 +0100

    I'm working on some edit.

--- DemoResizable/DemoResizable/MainPage.xaml.cs
+++ DemoResizable/DemoResizable/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+
 namespace DemoResizable
 {
     public partial class MainPage : ContentPage
@@ -16,6 +17,8 @@ namespace DemoResizable
             var assembly = typeof(MainPage);
 
             SplashScreenImage.Source = ImageSource.FromResource("DemoResizable.Assets.Images.splashpage.png", assembly);
+            int ScreenWidth = (int)Resources.DisplayMetrics.WidthPixels;
+            int ScreenHeight = (int)Resources.DisplayMetrics.HeightPixels;
         }
     }
 }
